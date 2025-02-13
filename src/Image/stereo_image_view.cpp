8036709cc801e9451919eefd715e0f262433b0a9
CarlosMelloeSouza <caca.stuart@hotmail.com>
2025-02-13 11:37:25 -0300

    feat: args camera info on view node

--- src/Image/stereo_image_view.cpp
+++ src/Image/stereo_image_view.cpp
@@ -15,7 +15,7 @@
 
 class StereoImageView : public rclcpp::Node {
 public:
-    StereoImageView()
+    StereoImageView(sensor_msg::msg::CameraInfo info_left, sensor_msg::msg::CameraInfo info_right)
         : Node("stereo_image_view") {
         // Read the parameter for show_window_ from the launch file
         this->declare_parameter<bool>("show_window", false);  // Declare the parameter with a default value
@@ -23,9 +23,13 @@ public:
         this->declare_parameter<bool>("rectificate_image",false);
         this->get_parameter("rectificate_image",rectificate_);
 
+        left_camera_info = info_left
+        right_camera_info = info_right
+
         // Initialize subscribers
         left_sub_ = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::Image>>(this, "/camera_1/image_raw");
         right_sub_ = std::make_shared<message_filters::Subscriber<sensor_msgs::msg::Image>>(this, "/camera_2/image_raw");
+
 
         // Initialize synchronizer with ApproximateTime policy
         sync_ = std::make_shared<message_filters::Synchronizer<SyncPolicy>>(
@@ -100,6 +104,7 @@ private:
         response->success = true;
         response->message = rectificate_ ? "image rectificated":"image raw";
 
+
     }
 
     std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Image>> left_sub_;
@@ -107,13 +112,43 @@ private:
     std::shared_ptr<message_filters::Synchronizer<SyncPolicy>> sync_;
     rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr service_show_window;
     rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr service_rectificate_image;
+    sensor_msg::msg::CameraInfo left_camera_info;
+    sensor_msg::msg::CameraInfo right_camera_info;
     bool show_window_; // Controle da exibição da janela
     bool rectificate_; // Controle da rectificaçao da imagem 
 };
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<StereoImageView>());
+    if (argc<3) {
+        RCLCPP_ERROR(rclcpp::get_loger("rclcpp"),"Usage: StereoImageView_node <left_info_topic> <right_info_topic>")
+    }
+    std::string left_info_topic = argv[1];
+    std::string right_info_topic = argv[2];
+
+    auto left_camera_info_received = false;
+    auto right_camera_info_received = false;
+
+    auto info_node = std::make_shared<rclcpp::Node>("camera_info_subscriber");
+    right_camera_info_received = rclcpp::wait_for_message<sensor_msgs::msg::CameraInfo>(right_camera_info,info_node,right_info_topic, std::chrono::seconds(5));
+    left_camera_info_received = rclcpp::wait_for_message<sensor_msgs::msg::CameraInfo>(left_camera_info,info_node,left_info_topic, std::chrono::seconds(5));
+    
+    
+    if(left_camera_info_received && right_camera_info_received){
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Camera info received");
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Left info topic: %s", left_info_topic.c_str());
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Right info topic: %s", right_info_topic.c_str());
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Right Projection Matrix P[3] value: %f", right_camera_info.p[3]);
+    }
+    else
+    {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "No camera info provided");
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Left info topic: %s", left_info_topic.c_str());
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Right info topic: %s", right_info_topic.c_str());
+    }
+
+
+    rclcpp::spin(std::make_shared<StereoImageView>(left_camera_info,right_camera_info));
     rclcpp::shutdown();
     return 0;
 }
