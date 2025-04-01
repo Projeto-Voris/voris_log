219e88b46e52bf6eed83da9ba6db7e41e47aff81
daniel <daniel_regner@hotmail.com>
2025-04-01 17:05:32 -0300

    feat: add namespace to rescale

--- launch/rescale.launch.py
+++ launch/rescale.launch.py
@@ -1,32 +1,37 @@
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
     # Define the ImageRescaler node
     return LaunchDescription([
-    Node(
-        package='voris_log', # Replace with your package name
-        executable='rescale',   # Name of the executable (from CMakeLists.txt)
-        name='image_l',         # Node name
-        output='screen',               # Print output to the screen
-        remappings=[
-            # Remap the subscriber topic
-            ('image_in','/SM2/left/image_raw'),
-            # Remap the publisher topic
-            ('image_out','/SM2/left/image_rescaled')
-        ]
+    DeclareLaunchArgument('namespace', default_value='SM2', description='namespace of node'),
+        Node(
+            package='voris_log', # Replace with your package name
+            executable='rescale',   # Name of the executable (from CMakeLists.txt)
+            name='image_l',        # Node name
+            namespace=LaunchConfiguration('namespace'),         # Namespace for the node
+            output='screen',               # Print output to the screen
+            remappings=[
+                # Remap the subscriber topic
+                ('image_in','/SM2/left/image_raw'),
+                # Remap the publisher topic
+                ('image_out','/SM2/left/image_rescaled')
+            ]
 
-    ),
-    Node(
-        package='voris_log', # Replace with your package name
-        executable='rescale',   # Name of the executable (from CMakeLists.txt)
-        name='image_r',         # Node name
-        output='screen',               # Print output to the screen
-        remappings=[
-            # Remap the subscriber topic
-            ('image_in','/SM2/right/image_raw'),
-            # Remap the publisher topic
-            ('image_out','/SM2/right/image_rescaled')
-        ]
-        )
-])
+        ),
+        Node(
+            package='voris_log', # Replace with your package name
+            executable='rescale',   # Name of the executable (from CMakeLists.txt)
+            name='image_r',         # Node name
+            namespace=LaunchConfiguration('namespace'),         # Namespace for the node
+            output='screen',               # Print output to the screen
+            remappings=[
+                # Remap the subscriber topic
+                ('image_in','/SM2/right/image_raw'),
+                # Remap the publisher topic
+                ('image_out','/SM2/right/image_rescaled')
+            ]
+            )
+    ])
